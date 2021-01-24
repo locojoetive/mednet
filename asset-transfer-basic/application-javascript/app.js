5c0352432ff31747c48f0da9af9e31c6e9493660
locojoetive <johannes.mic@gmail.com>
2021-01-24 15:47:16 +0100

    adds rest endpoint from backend to blockchain

--- asset-transfer-basic/application-javascript/app.js
+++ asset-transfer-basic/application-javascript/app.js
@@ -21,7 +21,7 @@ function prettyJSONString(inputString) {
 
 let contract;
 
-async function main() {
+async function main(asyncMethod) {
 	try {
 		// build an in memory object with the network configuration (also known as a connection profile)
 		const ccp = buildCCPOrg1();
@@ -61,6 +61,7 @@ async function main() {
 
 			// Get the contract from the network.
 			contract = network.getContract(chaincodeName);
+			await asyncMethod(contract);
 		} finally {
 			// Disconnect from the gateway when the application is closing
 			// This will close all connections to the network
@@ -114,87 +115,73 @@ var corsOptions = {
 
 app.use(cors(corsOptions))
 
-app.listen(8080, () => {
-	main().then(v => {
-		console.log('Server started!');
-	});
-});
 
-async function GetAllAssets(contract) {
-	console.log('\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger');
-	let result = await contract.evaluateTransaction('GetAllAssets');
-	console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+
+async function authenicate(asyncMethod) {
+	try {
+		// build an in memory object with the network configuration (also known as a connection profile)
+		const ccp = buildCCPOrg1();
+
+		// build an instance of the fabric ca services client based on
+		// the information in the network configuration
+		const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
+
+		// setup the wallet to hold the credentials of the application user
+		const wallet = await buildWallet(Wallets, walletPath);
+
+		// in a real application this would be done on an administrative flow, and only once
+		await enrollAdmin(caClient, wallet, mspOrg1);
+
+		// in a real application this would be done only when a new user was required to be added
+		// and would be part of an administrative flow
+		await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
+
+		// Create a new gateway instance for interacting with the fabric network.
+		// In a real application this would be done as the backend server session is setup for
+		// a user that has been verified.
+		const gateway = new Gateway();
+
+		try {
+			// setup the gateway instance
+			// The user will now be able to create connections to the fabric network and be able to
+			// submit transactions and query. All transactions submitted by this gateway will be
+			// signed by this user using the credentials stored in the wallet.
+			await gateway.connect(ccp, {
+				wallet,
+				identity: org1UserId,
+				discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+			});
+
+			// Build a network instance based on the channel where the smart contract is deployed
+			const network = await gateway.getNetwork(channelName);
+
+			// Get the contract from the network.
+			contract = network.getContract(chaincodeName);
+			if (asyncMethod)
+				await asyncMethod(contract);
+		} finally {
+			// Disconnect from the gateway when the application is closing
+			// This will close all connections to the network
+
+		}
+	} catch (error) {
+		console.error(`******** FAILED to run the application: ${error}`);
+	}
+}
+
+async function GetAllAssets() {
+	const result = await contract.evaluateTransaction('GetAllAssets');
 	return result;
 }
 
+app.listen(8080, () => {
+	authenicate()
+		.then( v => console.log('Server started!'))
+		.catch(v => console.log("Something's wrong in the jellies....."));
+});
+
 app.route('/api/assets').get(async (req, res) => {
-	console.log("request received");
-	res.send([
-		{
-			key: "asset1",
-			record: {
-			id: "asset1",
-			color: "blue",
-			size: 5,
-			owner: "Tomoko",
-			appraisedValue: 300,
-			docType: "asset"
-			}
-		},
-		{
-			key: "asset2",
-			record: {
-			id: "asset2",
-			color: "red",
-			size: 5,
-			owner: "Brad",
-			appraisedValue: 400,
-			docType: "asset"
-			}
-		},
-		{
-			key: "asset3",
-			record: {
-			id: "asset3",
-			color: "green",
-			size: 10,
-			owner: "Jin Soo",
-			appraisedValue: 500,
-			docType: "asset"
-			}
-		},
-		{
-			key: "asset4",
-			record: {
-			id: "asset4",
-			color: "yellow",
-			size: 10,
-			owner: "Max",
-			appraisedValue: 600,
-			docType: "asset"
-			}
-		},
-		{
-			key: "asset5",
-			record: {
-			id: "asset5",
-			color: "black",
-			size: 15,
-			owner: "Adriana",
-			appraisedValue: 700,
-			docType: "asset"
-			}
-		},
-		{
-			key: "asset6",
-			record: {
-			id: "asset6",
-			color: "white",
-			size: 15,
-			owner: "Michel",
-			appraisedValue: 800,
-			docType: "asset"
-			}
-		}
-	]);
+	const result = JSON.parse((await GetAllAssets()).toString());
+	console.log(result);
+	res.send(result);
 });
